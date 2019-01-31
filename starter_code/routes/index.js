cb7d46ee160cf0008ec0c303eb336f9fede0df45
Alec Budd <abudd1094@gmail.com>
2019-01-31 15:22:55 +0100

    nice

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+  .then(movies => {
+    res.render("movies", {movies: movies});
+    console.log(movies);
+  })
+  .catch(error => {
+    console.log(error)
+  });
+ 
+ });
 
 module.exports = router;
